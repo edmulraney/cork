0d39862a2869e5af6fdfe455e08ea4b63cec4159
Edward Mulraney <edmulraney@gmail.com>
2020-02-10 13:37:13 +0000

    tree contains just html elements, no components, expanded inline

--- src/create-tree.js
+++ src/create-tree.js
@@ -4,6 +4,8 @@ const isTextNode = element => element !== null && element.indexOf('<') === -1
 const isCloseElement = element => element.indexOf('</') !== -1
 const isOpenElement = element => element.indexOf('>') !== -1 && element.indexOf('/>') === -1
 const isSelfCloseElement = element => element !== null && element.indexOf('/>') !== -1
+const isOpenComponent = element => element !== null && /(<[A-Z])/.test(element) && element.indexOf('/>') === -1
+
 const getElementName = str => {
   const spaceIndex = str.indexOf(' ')
   const hasSpace = spaceIndex !== -1
@@ -17,9 +19,25 @@ const getProps = element => {
   propsArray.forEach((prop, index) => (index % 2 === 0) && (props[prop.trim()] = getProp(propsArray[index + 1])))
   return props
 }
-export const createTree = html => {
+const createNode = (element, Components) => {
+  const props = isTextNode(element) ? ({ nodeValue: element })  : getProps(element)
+  if (isOpenComponent(element)) {
+    return Components[getElementName(element)](props)
+  }
+
+  const node = {
+    debug: element,
+    type: isTextNode(element) ? 'text' : getElementName(element),
+    props: {
+      children: [],
+      ...props,
+    }
+  }
+  return node
+}
+export const createTree = (html, Components) => {
   const elements = getElements(html)
-  const rootElement = { type: 'root', props: {}, children: [] }
+  const rootElement = { type: 'root', props: { children: [] } }
   const stack = [rootElement]
   let elementIndex = 0
   
@@ -29,25 +47,21 @@ export const createTree = html => {
     const isEmptyNode = element.trim() === ''
     if (isEmptyNode) continue
 
-    const node = {
-      type: isTextNode(element) ? 'text' : getElementName(element),
-      children: [],
-      props: isTextNode(element) ? ({ nodeValue: element }) : getProps(element),
-    }    
-    
-    if (isSelfCloseElement(element) || isTextNode(element)) {
-      parent.children.push(node)
-      continue
-    }
-    
     if (isCloseElement(element)) {
       stack.pop()
       parent = stack[stack.length - 1]
       continue
     }
+
+    const node = createNode(element, Components)
+    
+    if (isSelfCloseElement(element) || isTextNode(element)) {
+      parent.props.children.push(node)
+      continue
+    }
     
     if (isOpenElement(element)) {
-      parent.children.push(node)
+      parent.props.children.push(node)
       stack.push(node)
       continue
     }
@@ -55,7 +69,6 @@ export const createTree = html => {
     
   }  
   console.log(JSON.stringify(stack[0], null, 2))
-  return stack[0].children[0] // ignore root container
+  return stack[0].props.children[0] // ignore root container
 }
 
-createTree('<div><h1>hello<b>a</b></h1></div>')
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import { createTree } from './create-tree.js'
 
+const hasComponent = html => /(<[A-Z])/.test(html)
+
+const expandTree = i => i
+// const expandTree = (root) => {
+//   const type = root.type
+//   if (Array.isArray(root)) return root.map(expandTree)
+//   const isHtmlElement = typeof type === 'string'
+
+//   const component = isHtmlElement ? root : type(root.props)
+//   if (component.props.children) {
+//     component.props.children.forEach((child, idx) => {
+//       if (child.type !== 'string') {
+//         component.props.children[idx] = expandTree(component.props.children[idx])
+//       }
+//     })
+//   }
+//   return component
+// }
+
+// README: it is correctly loggin [Object object] because we've not build props handling into html`` yet :)
+let depth = 0
 const render = (root, container) => {
+  depth++
   const element =
     root.type == 'text'
       ? document.createTextNode('')
@@ -10,47 +32,93 @@ const render = (root, container) => {
   const isProp = key => key !== 'children'
   const props = Object.keys(root.props).filter(isProp)
   props.forEach(name => element[name] = root.props[name])
-  console.log(element)
-  root.children.forEach(child => render(child, element))
+  if (depth === 1) {
+    console.log(123892183921839218, root)
+    console.log(JSON.stringify(root, null, 2))
+  }
+  root.props.children.forEach(child => render(child, element))
   container.appendChild(element)
 }
 
-const hasComponent = () => false
 const toStatic = (statics, dynamics) => {
+  console.log({statics, dynamics})
   let result = ''
   for (let [index, staticPart] of statics.entries()) {
     const dynamicPart = dynamics[index]
     result += staticPart
-    if (dynamicPart) result += dynamicPart
+    if (dynamicPart) {
+      console.log({dynamicPart})
+      result += dynamicPart
+    }
   }
   return result
 }
 
 const html = (statics, ...dynamics) => {
   const parsed = toStatic(statics, dynamics)
-  if (!hasComponent(parsed)) return createTree(parsed)
+  if (!hasComponent(parsed)) return expandTree(createTree(parsed))
   return (...Components) => {
     const components = Components.reduce((factories, factory) => ({ ...factories, [factory.name]: factory } ), {})
-    return createTree(parsed, components)
+    return expandTree(createTree(parsed, components))
   }
 }
 
-const test = html`<div>hello</div>`
-console.log(test)
-render(test, document.getElementById('app'))
+function Title (props) { return html`<h1>${props.children}</h1>` }
+
+// const test = html`<div>hello</div>`
+// const test2 = html`<div><h1>hihi</h1></div>`
+const test2 = html`<div><Title>hihi</Title></div>`(Title)
+// console.log(test)
+console.log(test2)
+render(test2, document.getElementById('app'))
 /*
-`<div>hello</div>`
+`<div><Title>hello</Title></div>`
 ---
+html()
+->
+createTree()
 {
-  type: 'div',
-  ref: document.createElement('div'),
-  props: {},
-  children: {
-    type: '',
-    ref: document.createTextNode(''),
-    props: {
-      nodeValue: 'hello'
-    },
+  "type": "div",
+  "props": {
+    "children": [
+      {
+        "type": Title,
+        "props": {
+          "children": [
+            {
+              "type": "text",
+              "props": {
+                "nodeValue": "hihi",
+                "children": []
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
+expandTree()
+{
+  "type": "div",
+  "props": {
+    "children": [
+      {
+        "type": "h1",
+        "props": {
+          "children": [
+            {
+              "type": "text",
+              "props": {
+                "nodeValue": "hihi",
+                "children": []
+              }
+            }
+          ]
+        }
+      }
+      }
+    ]
   }
 }
 ---
@@ -58,7 +126,7 @@ tree[0].appendChild(tree[0].children[0...])
 ---
 */
 
-
+/*
 const TodoItem = props => html`<li>${props.todo.title}</li>`
 
 const TodoList = props => {
@@ -82,3 +150,4 @@ const App = props => {
     <TodoFeature />
   `(TodoFeature)
 }
+*/
